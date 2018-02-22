eb5ec29a78818b525c27c67aed525ca2a0e4baa0
Matthew Hu <matthu24@gmail.com>
2018-02-22 11:47:42 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+  entry: './entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
